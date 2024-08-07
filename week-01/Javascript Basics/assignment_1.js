76eceb5b3c2dc65a4b8aa6e380b878c9c42862c6
Mohit <mohitkhairnar28@gmail.com>
2024-08-07 22:12:28 +0530

    Week 1 JS Basics Initial Commit

---
+++ week-01/Javascript Basics/assignment_1.js
@@ -0,0 +1,11 @@
+// Assignment 1
+// Write a function sum that finds the sum of two numbers.
+// Side quest - Try passing in a string instead of a number and see what happens?
+
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(sum(5, 2)); //7
+console.log(sum("Cohort", 3)); // Cohort3
+console.log(sum("Java", "Script")); // JavaScript
